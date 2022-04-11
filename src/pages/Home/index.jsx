ed0b28d28cb70f4c52205f85ca49caf89cb02d8f
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-11 13:23:30 -0300

    useState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,20 @@
+import React, { useState } from "react";
 import './style.css'
 import { Card } from '../../components/Card'
 
 export function Home() {
-  
+  const [studentName, setStudentName] = useState("");
   return (
     <div className="container">
-    <h1>Lista de Presença</h1>
-    <input type="text" placeholder="Digite o nome..." />
+    <h1>Nome: {studentName}</h1>
+    <input type="text" placeholder="Digite o nome..."
+    onChange={(e) => setStudentName(e.target.value)} />
     <button type="button">Adicionar</button>
-
-    <Card name="Rodrigo" time="10:55:25" />
-    <Card />
     
+    <Card name="Rodrigo" time="10:55:25" />
+    <Card name="João" time="11:00:10"/>
+    <Card name="Ana" time="12:55:25" />
+    <Card name="Joaquim" time="21:00:10"/>
     
 
     </div>
